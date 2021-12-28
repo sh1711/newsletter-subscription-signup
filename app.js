2d8c3435b42b1cff9f72a64ab33661620df99484
Shine Htet <sh10097133@gmail.com>
2021-12-28 19:21:40 +0630

    final commit

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.post("/failure", function (req, res) {
     res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("server start");
 });
 
